116f734726f4b7a6544ee5a7dec19f4851f54a30
conniepocky <c.amer@icloud.com>
2019-03-25 18:44:04 +0000

    added files

--- menu.py
+++ menu.py
@@ -2,6 +2,7 @@ import time
 
 def chatbot():
     import main
+
 def game():
     import game
 
@@ -14,16 +15,13 @@ def menu():
             Chatbot - 1
             Cool Game -  2
             Exit :( - 3
-            
             """))
 
 
         if choice == 1:
             chatbot()
-            break
         elif choice == 2:
             game()
-            break
         elif choice == 3:
             print("BYEEE!")
             break
